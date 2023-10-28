c1ef31705b402e99178ad83f9a8c4cca6aff4da3
Anush79 <barbieworld1056.a@gmail.com>
2023-10-28 16:13:33 +0530

    Fix: name

--- src/features/Dashboard.jsx
+++ src/features/Dashboard.jsx
@@ -4,22 +4,16 @@ import { useSelector } from "react-redux";
 export default function Dashboard() {
   const { patients } = useSelector((state) => state?.patients);
   const { wards } = useSelector(state => state?.wards);
-  // const totalCapacity  = wards?.reduce((acc, curr)=>acc?.capacity+curr?.capacity,0)
-  // const wards = [2,4,6,7,8,9];
+ 
   const totalCapacity = wards?.reduce((acc, curr) => {
     return Number(curr.capacity) + Number(acc)
   }, 0)
-  
-  // useEffect(() => {
 
-  // }, [wards])
   return (
     <>
-      <h2>Welcome to Hospital Management System</h2>
+      <h2>Welcome to Patient Management System</h2>
       <p>
-        Users can view hospital-wide statistics, including the total number of
-        patients, the current occupancy rate, the average length of stay, and
-        the top-performing ward based on various criteria.
+        Users can view hospital-wide data.
       </p>
       <p>
         <p>
@@ -28,6 +22,7 @@ export default function Dashboard() {
         <p>
           Current Occupancy Rate: <b>{Math.ceil( (patients?.length/totalCapacity)*100)}%</b>
         </p>
+        
       </p>
     </>
   );
